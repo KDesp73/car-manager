798d8afc31971561caa3d41839a9c4c8dd323bac
ThanasisGeorg <thanasisgeorg03@gmail.com>
2024-11-04 12:10:54 +0200

    changes

--- src/main/java/io/github/dmgtechlabs/App.java
+++ src/main/java/io/github/dmgtechlabs/App.java
@@ -23,11 +23,12 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-		var manufacturers = Manufacturer.selectAll();
-
-		for (Manufacturer m : manufacturers) {
-			System.out.println(m.toString());
-		}
+//		Customer c1 = new Customer(1234, "a23df-fah43-pi3jh-asd64", new Person(1234, "Athanasios", "Georgalis", "thanasisgeorg03@gmail.com", 2003, Person.Gender.MALE));		
+//		Customer c2 = new Customer(5678, "b29kf-mfgh3-poi21-nuvi7", new Person(5678, "Konstantinos", "Despoinidis", "kdesp73@gmail.com", 2003, Person.Gender.MALE));
+//		
+//		c1.insert();
+//		c2.insert();
+		
 //		runGUI();
 	}
 
